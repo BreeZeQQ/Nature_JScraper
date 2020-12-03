890ce76b1ebe16b906f1198df4a8d968fdc2b736
Berk Can <31792851+BreeZeQQ@users.noreply.github.com>
2020-12-03 22:15:53 +0300

    Update install.py

--- install.py
+++ install.py
@@ -3,8 +3,9 @@ import os
 
 os.system("git clone https://github.com/GerbenJavado/LinkFinder.git")
 os.system("cp LinkFinder/README.md .")
-os.system("python LinkFinder/setup.py install")
-os.system("pip3 install pyfiglet")
+os.system("python3 LinkFinder/setup.py install")
+os.system("pip3 install -r  LinkFinder/requirements.txt")
+os.system("pip3 install pyfiglet argparse html==1.7")
 os.system("rm -rf build/ README.md dist/ LinkFinder.egg-info/")
 os.system("mv LinkFinder/linkfinder.py LinkFinder/template.html .")
 os.system("rm -rf LinkFinder")
